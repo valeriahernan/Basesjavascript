42091acca53d1bce098a91de9842f70fb36060eb
Valeria Hernández Montesino <valeriahernan@gmail.com>
2025-11-06 20:54:20 -0300

    ok

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,9 +5,20 @@ let nombre = "Valeria";
 console.log(nombre);
 
 let edad = 25;
-
 edad = 30;
 console.log(edad);
 
 const pais = "Chile";
-pais = "Brasil";
+// pais = "Brasil"; no se puede modificar una constante
+
+/* reglas para nombrar variables */
+//ejemplos válidos de nombres de variables
+let apellido;
+let _contador;
+let $precioTotal;
+let numeroDeEstudiantes = 30
+
+//tipos de datos de las variables
+
+let saludo = "Hola"; //string o textoos
+let esEstudiante = true;
