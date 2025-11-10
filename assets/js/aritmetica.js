4c4a660de97a519ba56cd16eaca99515fb54060e
Valeria Hernández Montesino <valeriahernan@gmail.com>
2025-11-10 19:46:07 -0300

    ARITMETICA

---
+++ assets/js/aritmetica.js
@@ -0,0 +1,12 @@
+// operaciones aritmeticas
+
+let a = 10;
+let b = 3;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b); // multiplicación
+console.log(a / b); // división
+console.log(a % b); // módulo ó el resto de la división
+console.log(a ** b); // potencias a elevadas a B
+
